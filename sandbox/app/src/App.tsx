080d8ca02af41867a1dca08553acf9e67dd60241
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-11-07 12:33:51 +0100

    ✨ feat(app): dummy layout
    So as to have something ressembling an app with header, footer and
    sidebars.
    
    Each area contains a dummy centered spinner. Nothing is loaded. Page is
    static.

--- sandbox/app/src/App.tsx
+++ sandbox/app/src/App.tsx
@@ -1,10 +1,51 @@
 import { Spinner } from "./components";
 
+const CenteredSpinner = () => (
+    <div className="grid h-full place-items-center">
+        <Spinner />
+    </div>
+);
+
+const Header = () => (
+    <header className="h-20 w-full bg-blue-300">
+        <CenteredSpinner />
+    </header>
+);
+const Sidebar = () => (
+    <aside className="h-full w-72 bg-orange-300/50">
+        <CenteredSpinner />
+        <CenteredSpinner />
+        <CenteredSpinner />
+        <CenteredSpinner />
+        <CenteredSpinner />
+        <CenteredSpinner />
+    </aside>
+);
+const Main = () => (
+    <main className="h-full w-full">
+        <CenteredSpinner />
+    </main>
+);
+const Body = () => (
+    <div className="flex h-full w-full flex-row text-gray-700 dark:text-gray-200">
+        <Sidebar />
+        <Main />
+        <Sidebar />
+    </div>
+);
+const Footer = () => (
+    <header className="h-20 w-full bg-blue-300">
+        <CenteredSpinner />
+    </header>
+);
+
 function App() {
     return (
-        <div className="grid h-screen place-items-center">
-            <Spinner />
-        </div>
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
     );
 }
 
