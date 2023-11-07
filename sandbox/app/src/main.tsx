1b6c5d925691fc412ae527fba9242aa88eec360c
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-11-07 01:28:05 +0100

    ✨ feat(app): add background color
    Background is set for both light and dark mode.

--- sandbox/app/src/main.tsx
+++ sandbox/app/src/main.tsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <div className="h-screen w-full overflow-hidden">
+        <div className="h-screen w-full overflow-hidden bg-gray-200 dark:bg-gray-800">
             <RouterProvider router={router} />
         </div>
     </React.StrictMode>,
